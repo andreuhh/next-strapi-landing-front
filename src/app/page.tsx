f63e58b9f26ba39d5a849663aa5a7bd2cf1c4f7d
Andrea Guffi <andreaguffi92@gmail.com>
2024-07-30 22:45:36 +0200

    hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,38 @@
-import { Button } from "@/components/ui/button";
+import { HeroSection } from "@/components/ui/custom/HeroSection";
+import { flattenAttributes } from "@/lib/utils";
+import qs from "qs";
+
+const homePageQuery = qs.stringify({
+  populate: {
+    blocks: {
+      populate: {
+        image: {
+          fields: ["url", "alternativeText"]
+        },
+        link: {
+          populate: true
+        },
+      },
+    },
+  },
+});
 
 async function getStrapiData(path: string) {
   const baseUrl = "http://localhost:1337";
+
+  const url = new URL(path, baseUrl);
+  url.search = homePageQuery;
+
+  console.log(url.href)
+
   try {
-    const response = await fetch(baseUrl + path);
+    const response = await fetch(url.href, { cache: 'no-store' });
     const data = await response.json();
-    return data;
+
+    const flattenedData = flattenAttributes(data);
+    console.dir(flattenedData, { dept: null }); // complete consolle of data
+
+    return flattenedData;
   } catch (error) {
     console.error(error);
   }
@@ -15,14 +42,12 @@ export default async function Home() {
   const strapiData = await getStrapiData("/api/home-page");
   console.log(strapiData, 'here')
 
-  const { title, description } = strapiData.data.attributes;
+  const { title, description, blocks } = strapiData;
 
   return (
-    <div>
-      {title}
-      {description}
-      <Button>Click me</Button>
-    </div>
+    <main>
+      <HeroSection data={blocks[0]} />
+    </main>
 
   );
 }
